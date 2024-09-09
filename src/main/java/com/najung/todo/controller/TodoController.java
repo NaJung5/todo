8510736008b28980a2ee9991da39c67990629466
나정호 <njung2516@gmail.com>
2024-09-09 17:26:24 +0900

    리팩토링
    1. Controller
        - Operation를 사용해 해당 API가 무슨 역할을 하는지 설명
    2. Reqeust
        - Schema 를 통해 해당 필드의 설명을 추가

--- src/main/java/com/najung/todo/controller/TodoController.java
+++ src/main/java/com/najung/todo/controller/TodoController.java
@@ -12,6 +12,8 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import io.swagger.v3.oas.annotations.Operation;
+
 
 @Slf4j
 @RequiredArgsConstructor
@@ -21,6 +23,7 @@ public class TodoController {
 
     private final TodoService todoService;
 
+    @Operation(summary = "todo 조회", description = "userId를 받아 해당 유저가 작성한 todo의 리스트를 반환")
     @GetMapping
     public Page<TodoResponse> todo(@PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
                                    @RequestParam Long userId) {
@@ -28,6 +31,7 @@ public class TodoController {
         return todoService.searchTodo(userId, pageable).map(TodoResponse::from);
     }
 
+    @Operation(summary = "todo 저장", description = "userId를 받아 해당 유저가 작성한 todo를 저장")
     @PostMapping("/members/{memberId}/todos")
     public ResponseEntity<?> postTodo(@PathVariable Long memberId,
                                       @RequestBody TodoRequest todoRequest) {
@@ -35,6 +39,7 @@ public class TodoController {
         return ResponseEntity.ok("저장 되었습니다.");
     }
 
+    @Operation(summary = "todo 반복", description = "userId와 todoId 받아 해당 유저가 작성한 todo를 반복 저장")
     @PostMapping("/members/{memberId}/todo/{todoId}/multiple")
     public ResponseEntity<?> multipleTodo(@PathVariable Long memberId,
                                           @PathVariable Long todoId,
@@ -43,6 +48,7 @@ public class TodoController {
         return ResponseEntity.ok("저장 되었습니다.");
     }
 
+    @Operation(summary = "todo 업데이트", description = "userId와 todoId 받아 해당 유저가 작성한 todo를 업데이트")
     @PutMapping("/member/{memberId}/todo/{todoId}")
     public ResponseEntity<?> updateTodo(@PathVariable Long memberId,
                                         @PathVariable Long todoId,
@@ -51,6 +57,7 @@ public class TodoController {
         return ResponseEntity.ok("수정 되었습니다.");
     }
 
+    @Operation(summary = "마감일 업데이트", description = "userId와 todoId 받아 해당 유저가 작성한 todo의 마감일을 변경")
     @PatchMapping("/members/{memberId}/todo/{todoId}/due-date")
     public ResponseEntity<?> updateDueDate(@PathVariable Long memberId,
                                            @PathVariable Long todoId,
@@ -60,7 +67,7 @@ public class TodoController {
         return ResponseEntity.ok("일정이 수정되었습니다.");
     }
 
-
+    @Operation(summary = "todo 삭제", description = "userId와 todoId 받아 해당 유저가 작성한 todo를 삭제")
     @DeleteMapping("/delete/{todoId}/{memberId}")
     public ResponseEntity<?> deleteTodo(@PathVariable Long todoId,
                                         @PathVariable Long memberId) {
